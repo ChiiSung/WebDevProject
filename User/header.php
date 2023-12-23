01d6e22062aae0ebe7c724fd5653d06a242df250
ChiiSung <lingchiisung@gmail.com>
2023-12-23 16:32:07 +0800

    ad

--- User/header.php
+++ User/header.php
@@ -1,34 +1,34 @@
 <?php  echo '
 <nav class="navbar navbar-default navbar-expand-lg fixed-top custom-navbar">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="icon ion-md-menu"></span>
-        </button>
-        <img src="../images/logo.png" class="img-fluid nav-logo-mobile" alt="Company Logo">
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <div class="container">
-              <img src="../images/logo.png" class="img-fluid nav-logo-desktop" alt="Company Logo">
-            <ul class="navbar-nav ml-auto nav-right" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
-              <li class="nav-item nav-custom-link">
-                <a class="nav-link" href="index.php">Home <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-              <li class="nav-item nav-custom-link">
-                <a class="nav-link" href="product.php">Product <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-              <li class="nav-item nav-custom-link">
-                <a class="nav-link" href="event.php">Event <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-              <li class="nav-item nav-custom-link">
-                <a class="nav-link" href="aboutUs.php">About Us <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-              <li class="nav-item nav-custom-link">
-                <a class="nav-link" href="contactUs.php">Contact Us <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-              <li class="nav-item nav-custom-link btn btn-demo-small">
-                <a class="nav-link" href="#">Try for Free <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="icon ion-md-menu"></span>
+  </button>
+  <img src="../images/small_logo.png" class="img-fluid nav-logo-mobile" alt="Company Logo">
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="container">
+        <img src="../images/logo.png" class="img-fluid nav-logo-desktop" alt="Company Logo">
+      <ul class="navbar-nav ml-auto nav-right" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+        <li class="nav-item nav-custom-link">
+          <a class="nav-link" href="index.php">Home <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+        <li class="nav-item nav-custom-link">
+          <a class="nav-link" href="product.php">Product <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+        <li class="nav-item nav-custom-link">
+          <a class="nav-link" href="event.php">Event <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+        <li class="nav-item nav-custom-link">
+          <a class="nav-link" href="aboutUs.php">About Us <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+        <li class="nav-item nav-custom-link">
+          <a class="nav-link" href="contactUs.php">Contact Us <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+        <li class="nav-item nav-custom-link btn btn-demo-small">
+          <a class="nav-link" href="#">Try for Free <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     '
 ?>
